952142761211093f1424783eec120ad4f1a88ae7
bebinasainju1 <60206454+bebinasainju1@users.noreply.github.com>
2020-04-24 21:06:53 -0500

    Add files via upload

---
+++ Lab01.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Lab01
+{
+    public static void Main()
+    {
+        Console.WriteLine("CS 201 Restaurant Guide\n");
+        String response;
+        char s, f;
+        bool spicy,fancy;
+        
+        Console.Write("Do you like spicy food?(y/n):");
+
+        response = Console.ReadLine();//Console.ReadLine() provides the interface for reading data
+        s = response[0];//This is the first character of the string
+
+        if ((s == 'y') || (s == 'Y'))
+            spicy = true;
+        else
+            spicy = false;
+
+        Console.Write("Do you want to go to a fancy restaurant?(y/n):");
+        response = Console.ReadLine();
+        f = response[0];
+        fancy = ((f == 'y') || (f == 'Y'));
+        if (spicy)
+        {     //Blocks
+            if (fancy)
+                Console.WriteLine("I suggest you go to Thai Garden Palace.");
+            else
+                Console.WriteLine("I suggest you to go to Alberto's Tacqueria.");
+        }
+        else
+        {     //Blocks
+            if (fancy)
+                Console.WriteLine("I suggest you to go to Chez Paris.");
+            else
+                Console.WriteLine("I suggest you go to Joe's Diner.");
+        }
+    }
+}
+
+
+// Ans 2: Yes, both scenario are equivalent. In the first "if statement" is true or gives false value, it values false and in second condition the output eill be false else True.
+// Ans 5: The if and else block are commented as a block in the code.
+// Ans 6: The code is represented in nested if-else.
---
+++ Lab02.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+    public class Lab02
+    {
+        public static void Main()
+        {
+            double Single_Prescription_Glass = 40.00;
+            double Non_Prescription_Sunglass = 25.00;
+            double Anti_Glare_Coating = 12.50;
+            double Brown_Tint_Coating = 9.99;
+
+            Console.WriteLine("What kind of glasses would you like: ");//Console.WriteLine() is used for printing out the given msg
+            Console.WriteLine("1 -> Prescription, 2 -> Non-Prescription : ");
+
+        int  client_pref= int.Parse(Console.ReadLine());//Console.ReadLine() provides the interface for reading data
+            if (client_pref < 1 || client_pref > 2)
+            {
+                Console.WriteLine("1 -> Prescription, 2 -> Non-Prescription : ");
+            client_pref = int.Parse(Console.ReadLine());
+            }
+            double product_price = 0.0; //Double represents the fractional value
+
+                if (client_pref == 1)
+                product_price = Single_Prescription_Glass;
+                else
+                product_price = Non_Prescription_Sunglass;
+
+            Console.WriteLine("What kind of coating would you like: ");
+            Console.WriteLine("1 -> Anti-glare, 2 -> Brown tint : ");
+
+            client_pref = int.Parse(Console.ReadLine());
+
+            if (client_pref < 1 || client_pref > 2)
+            {
+                Console.WriteLine("1 -> Anti-glare, 2 -> Brown tint : ");
+            client_pref = int.Parse(Console.ReadLine());
+
+            }
+            if (client_pref == 1)
+            product_price = product_price + Anti_Glare_Coating;
+            else
+            product_price = product_price + Brown_Tint_Coating;
+
+            Console.WriteLine("\nYour total cost us is $" + product_price);
+            Console.ReadLine();
+        }
+    }
+
